96509695d407a51ff1d9c370e3b9e2dc56bf212c
Kevin Killick <caoimh@gmail.com>
2017-02-22 14:37:54 +0000

    Added Publish to Azure

---
+++ src/TrailerCheck/Migrations/20170220161706_InitialCreate.Designer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TrailerCheck.Data;
+
+namespace TrailerCheck.Migrations
+{
+    [DbContext(typeof(TrailerCheckContext))]
+    [Migration("20170220161706_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("TrailerCheck.Models.Owner", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<DateTime>("RegistrationDate");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Owner");
+                });
+
+            modelBuilder.Entity("TrailerCheck.Models.Registration", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("OwnerID");
+
+                    b.Property<int>("TrailerID");
+
+                    b.HasKey("ID");
+
+                    b.HasIndex("OwnerID");
+
+                    b.HasIndex("TrailerID");
+
+                    b.ToTable("Registration");
+                });
+
+            modelBuilder.Entity("TrailerCheck.Models.Trailer", b =>
+                {
+                    b.Property<int>("TrailerID");
+
+                    b.Property<string>("Description");
+
+                    b.Property<string>("Model");
+
+                    b.HasKey("TrailerID");
+
+                    b.ToTable("Trailer");
+                });
+
+            modelBuilder.Entity("TrailerCheck.Models.Registration", b =>
+                {
+                    b.HasOne("TrailerCheck.Models.Owner", "Owner")
+                        .WithMany("Registrations")
+                        .HasForeignKey("OwnerID")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("TrailerCheck.Models.Trailer", "Trailer")
+                        .WithMany("Registrations")
+                        .HasForeignKey("TrailerID")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ src/TrailerCheck/Migrations/20170220161706_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TrailerCheck.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owner",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    RegistrationDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owner", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trailer",
+                columns: table => new
+                {
+                    TrailerID = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Model = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trailer", x => x.TrailerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registration",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OwnerID = table.Column<int>(nullable: false),
+                    TrailerID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registration", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Registration_Owner_OwnerID",
+                        column: x => x.OwnerID,
+                        principalTable: "Owner",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Registration_Trailer_TrailerID",
+                        column: x => x.TrailerID,
+                        principalTable: "Trailer",
+                        principalColumn: "TrailerID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registration_OwnerID",
+                table: "Registration",
+                column: "OwnerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registration_TrailerID",
+                table: "Registration",
+                column: "TrailerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registration");
+
+            migrationBuilder.DropTable(
+                name: "Owner");
+
+            migrationBuilder.DropTable(
+                name: "Trailer");
+        }
+    }
+}
